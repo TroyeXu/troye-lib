7b34a2c2dd4f2a0665ff99821c5b479349760280
troye <troye@forfuns.net>
2020-09-24 14:41:07 +0800

    init

---
+++ dev/serve.vue
@@ -0,0 +1,17 @@
+<script>
+import Vue from 'vue'
+import { TroyelibSample } from '@/entry'
+
+export default Vue.extend({
+  name: 'ServeDev',
+  components: {
+    TroyelibSample,
+  },
+})
+</script>
+
+<template>
+  <div id="app">
+    <troyelib-sample />
+  </div>
+</template>
